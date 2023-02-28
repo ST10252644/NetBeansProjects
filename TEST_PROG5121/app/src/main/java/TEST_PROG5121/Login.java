c12bb2c0587dfae6af25e2635895652dc53026c5
Cherika Bodde <st10252644@vcconnect.edu.za>
2023-02-28 14:22:19 +0000

    Initial commit

---
+++ TEST_PROG5121/app/src/main/java/TEST_PROG5121/Login.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TEST_PROG5121;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Login {
+    
+   public String login = "I am from the class";
+    
+}
---
+++ TEST_PROG5121/app/src/test/java/TEST_PROG5121/LoginTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package TEST_PROG5121;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
